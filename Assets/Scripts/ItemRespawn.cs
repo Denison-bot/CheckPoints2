f6264fef55800d7f366e7defa34defba790887dc
Denison Percey <denisonipercey@outlook.com>
2022-01-27 15:42:00 -0400

    reset timer to 5 seconds

--- Assets/Scripts/ItemRespawn.cs
+++ Assets/Scripts/ItemRespawn.cs
@@ -24,6 +24,7 @@ public class ItemRespawn : MonoBehaviour
             {
                 this.gameObject.GetComponent<MeshRenderer>().enabled = true;
                 collected = false;
+                respawnTimer = 5.0f;
             }
        }        
     }
